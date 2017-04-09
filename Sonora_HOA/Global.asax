761ef75cb45afcdaaa0f75e51827aa3cf22b2b3a
Gibran Amparan <GibranAmparanD@hotmail.com>
2017-04-08 18:58:30 -0700

    Initial Commit

---
+++ Sonora_HOA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sonora_HOA.MvcApplication" Language="C#" %>
---
+++ Sonora_HOA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sonora_HOA.Startup))]
+namespace Sonora_HOA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
