94a327f223857190d20f1ab448089981f810f58f
Rangel <erick_1095@hotmail.com>
2017-04-11 13:11:05 -0700

    generados todos los modelos y sus relaciones

--- Sonora_HOA/Models/Visits.cs
+++ Sonora_HOA/Models/Visits.cs
@@ -7,5 +7,21 @@ namespace Sonora_HOA.Models
 {
     public class Visits
     {
+        public int visitid { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}",
+        ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}",
+        ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime arrivedDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}",
+        ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime departureDate { get; set; }
     }
 }
