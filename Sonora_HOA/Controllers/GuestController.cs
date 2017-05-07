96faff1098d29262cca1867546dd2a8530a03a6c
TIC-PROYECTOS1 <TIC-PROYECTOS1@LAPTOP-DIU0S8M8>
2017-05-07 16:17:37 -0700

    Trabajando en los bloqueos por permisos de acceso en roles

--- Sonora_HOA/Controllers/GuestController.cs
+++ Sonora_HOA/Controllers/GuestController.cs
@@ -66,7 +66,7 @@ namespace Sonora_HOA.Controllers
         }
 
         // GET: Guest/Edit/5
-        [Authorize(Roles = ApplicationUser.RoleNames.ADMIN)]
+        [Authorize(Roles = ApplicationUser.RoleNames.ADMIN + "," + ApplicationUser.RoleNames.OWNER)]
         public ActionResult Edit(string id)
         {
             if (id == null)
@@ -84,7 +84,7 @@ namespace Sonora_HOA.Controllers
         // POST: Guest/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [Authorize(Roles = ApplicationUser.RoleNames.ADMIN)]
+        [Authorize(Roles = ApplicationUser.RoleNames.ADMIN + "," + ApplicationUser.RoleNames.OWNER)]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,guestID,name,lastName")] Guest guest)
@@ -99,7 +99,7 @@ namespace Sonora_HOA.Controllers
         }
 
         // GET: Guest/Delete/5
-        [Authorize(Roles = ApplicationUser.RoleNames.ADMIN)]
+        [Authorize(Roles = ApplicationUser.RoleNames.ADMIN + "," + ApplicationUser.RoleNames.OWNER)]
         public ActionResult Delete(string id)
         {
             if (id == null)
@@ -115,7 +115,7 @@ namespace Sonora_HOA.Controllers
         }
 
         // POST: Guest/Delete/5
-        [Authorize(Roles = ApplicationUser.RoleNames.ADMIN)]
+        [Authorize(Roles = ApplicationUser.RoleNames.ADMIN + "," + ApplicationUser.RoleNames.OWNER)]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(string id)
