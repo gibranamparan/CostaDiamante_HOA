cb753ff7bb7ba661e1120587f9b7868417cbf29d
Gibran Amparan <GibranAmparanD@hotmail.com>
2017-07-04 18:32:01 -0700

    Se agrego un manual de usuario

--- Sonora_HOA/Controllers/HomeController.cs
+++ Sonora_HOA/Controllers/HomeController.cs
@@ -8,11 +8,10 @@ namespace Sonora_HOA.Controllers
 {
     public class HomeController : Controller
     {
-        /*
-        public ActionResult Index()
+        public ActionResult UserManual()
         {
             return View();
-        }*/
+        }
 
         public ActionResult About()
         {
